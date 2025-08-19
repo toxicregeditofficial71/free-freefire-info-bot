d752e94623386441093e166dd547169680225567
toxicregeditofficial71 <toxicregeditofficial@gmail.com>
2025-08-19 16:50:00 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -1,4 +1,5 @@
 
+
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -19,7 +20,7 @@ class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.api_url = "http://raw.thug4ff.com/info"
-        self.generate_url = "https://generatethug.onrender.com/generate"
+        self.generate_url = "https://profile-generator.up.railway.app/api/profile"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
         self.cooldowns = {}
