2507e92841b92e2091c961f3957121957fa27c9a
toxicregeditofficial71 <toxicregeditofficial@gmail.com>
2025-08-23 22:20:05 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -20,7 +20,7 @@ class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.api_url = "http://raw.thug4ff.com/info"
-        self.generate_url = "https://profile-generator.up.railway.app/api/profile"
+        self.generate_url = "https://genprofile-24nr.onrender.com/api/profile"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
         self.cooldowns = {}
